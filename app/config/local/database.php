f94ae55e8d6d717d0d83fabe06530311ee92c156
Kade Price <kade.price@planswift.com>
2014-12-17 20:00:41 -0700

    Pushing more vender files

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,24 +23,13 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'maladlist',
+			'username'  => 'root',
+			'password'  => 'Gr8stuff',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'homestead',
-			'username' => 'homestead',
-			'password' => 'secret',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
+		)
 
 	),
 
