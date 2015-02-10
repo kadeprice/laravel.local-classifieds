d975bd29fcb44cd0efceadec04c6bd5f39f58711
Kade Price <kade.price@planswift.com>
2015-02-10 13:41:29 -0700

    Adding the start of a template.

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,53 @@
+@include("layouts/head")
+<body data-spy="scroll" data-target="#topnav">
+    <div class="flash">
+        <!--Flash message-->
+        @if (Session::get('notification'))
+            <div id='myAlert' class='alert alert-warning alert-dismissable fade in' data-alert='alert'>
+                
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <div id="flash_message">{{ Session::get('notification') }} </div>
+            </div>
+        @endif
+    </div>
+
+<div class="navbar navbar-inverse navbar-fixed-top" id="topnav">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="images/expose-logo.png" alt="Expose" height="17px" /> </a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#home">Home</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#pricing">Pricing</a></li>
+                <li><a href="#clients">Clients</a></li>
+                <li><a href="#contact">Contact</a></li>
+
+            </ul>
+
+        </div>
+        <!--/.navbar-collapse -->
+    </div>
+</div>
+@yield('content')  
+
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+        {{ HTML::script('//code.jquery.com/jquery-2.1.1.min.js') }}
+        {{ HTML::script('//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/jquery-ui.min.js') }}
+        {{ HTML::script('bootstrap/js/bootstrap.min.js') }}
+<!--<script src="js/jquery.js"></script>-->
+</body>
+</html>
+
